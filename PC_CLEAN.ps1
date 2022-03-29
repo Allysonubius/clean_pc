9cfc73fb2b2fe4eff850b36fb2dab0a55129c9f8
Allyson de Oliveira Brandão <allysomoliveira7@gmail.com>
2022-03-29 17:01:11 -0300

    update - Converte USA para PT-BR

--- PC_CLEAN.ps1
+++ PC_CLEAN.ps1
@@ -4,11 +4,11 @@
 	Write-Output $WinTemp = "c:\Windows\Temp\*"
 	
 #1#	Empty Recycle Bin #
-	write-Host "Emptying Recycle Bin." -ForegroundColor Cyan 
+	write-Host "Limpando a pasta Recycle Bin." -ForegroundColor Cyan 
 	$objFolder.items() | ForEach-Object{ remove-item $_.path -Recurse -Confirm:$false}
 	
 #2# Remove Temp
-	write-Host "Removing Temp" -ForegroundColor Green
+	write-Host "Removendo lixo da pasta Temp" -ForegroundColor Green
     Set-Location “C:\Windows\Temp”
 	Remove-Item * -Recurse -Force -ErrorAction SilentlyContinue
 
@@ -22,11 +22,11 @@
     Remove-Item “.\*\Appdata\Local\Temp\*” -Recurse -Force -ErrorAction SilentlyContinue
 	
 #3# Running Disk Clean up Tool 
-	write-Host "Finally now , Running Windows disk Clean up Tool" -ForegroundColor Cyan
+	write-Host "Executando a ferramenta de limpeza das sujeiras do disco do windows" -ForegroundColor Cyan
 	cleanmgr /sagerun:1 | out-Null 
 	
 	$([char]7)
 	Start-Sleep 3
-	write-Host "I finished the cleanup task,Bye Bye " -ForegroundColor Yellow 
+	write-Host "Finalizado tarefa de limpeza" -ForegroundColor Yellow 
 	Start-Sleep 3
 ##### End of the Script ##### 
